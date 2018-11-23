d7aaf5835a674812cd0cd1883ca77ba677933991
manchalajahnavi <jahnavimanchala89@gmail.com>
2018-11-23 11:54:57 +0530

    commit

--- sub_header.php
+++ sub_header.php
@@ -16,48 +16,7 @@
                     </div>
 
                     <div class="dropdown for-message">
-                        <!--   <button class="btn btn-secondary dropdown-toggle" type="button" id="message" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="ti-email"></i>
-                         <span class="count bg-primary">4</span>
-                        </button>-->
-<!--
-                        <div class="dropdown-menu" aria-labelledby="message">
-                            <p class="red">You have 4 Mails</p>
-                            <a class="dropdown-item media bg-flat-color-1" href="#">
-                                <span class="photo media-left"><img alt="avatar" src="images/avatar/1.jpg"></span>
-                                <span class="message media-body">
-                                    <span class="name float-left text-white">Jonathan Smith</span>
-                                    <span class="time float-right text-white">Just now</span>
-                                    <p class="text-light">Hello, this is an example msg</p>
-                                </span>
-                            </a>
-                            <a class="dropdown-item media bg-flat-color-4" href="#">
-                                <span class="photo media-left"><img alt="avatar" src="images/avatar/2.jpg"></span>
-                                <span class="message media-body">
-                                    <span class="name float-left text-white">Jack Sanders</span>
-                                    <span class="time float-right text-white">5 minutes ago</span>
-                                    <p class="text-light">Lorem ipsum dolor sit amet, consectetur</p>
-                                </span>
-                            </a>
-                            <a class="dropdown-item media bg-flat-color-5" href="#">
-                                <span class="photo media-left"><img alt="avatar" src="images/avatar/3.jpg"></span>
-                                <span class="message media-body">
-                                    <span class="name float-left text-white">Cheryl Wheeler</span>
-                                    <span class="time float-right text-white">10 minutes ago</span>
-                                    <p class="text-light">Hello, this is an example msg</p>
-                                </span>
-                            </a>
-                            <a class="dropdown-item media bg-flat-color-3" href="#">
-                                <span class="photo media-left"><img alt="avatar" src="images/avatar/4.jpg"></span>
-                                <span class="message media-body">
-                                    <span class="name float-left text-white">Rachel Santos</span>
-                                    <span class="time float-right text-white">15 minutes ago</span>
-                                    <p class="text-light">Lorem ipsum dolor sit amet, consectetur</p>
-                                </span>
-                            </a>
-                            <a href="#" class="float-right text-white p-1">See all</a>
-                        </div>
--->
+                        
                     </div>
                 </div>
             </div>
@@ -77,27 +36,7 @@
                     </div>
                 </div>
 
-<!--
-                <div class="language-select dropdown" id="language-select">
-                    <a class="dropdown-toggle" href="#" data-toggle="dropdown" id="language" aria-haspopup="true" aria-expanded="true">
-                        <i class="flag-icon flag-icon-us"></i>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="language">
-                        <div class="dropdown-item">
-                            <span class="flag-icon flag-icon-fr"></span>
-                        </div>
-                        <div class="dropdown-item">
-                            <i class="flag-icon flag-icon-es"></i>
-                        </div>
-                        <div class="dropdown-item">
-                            <i class="flag-icon flag-icon-us"></i>
-                        </div>
-                        <div class="dropdown-item">
-                            <i class="flag-icon flag-icon-it"></i>
-                        </div>
-                    </div>
-                </div>
--->
+
 
             </div>
         </div>
